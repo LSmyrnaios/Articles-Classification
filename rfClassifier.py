0141388e00884e7686c42607645f31bf1d7e7e63
LSmyrnaios <lsmyrnaios@gmail.com>
2018-04-23 10:40:27 +0300

    - Introduce spliting of dataSet in "rfClassifier.py" for better training-prediction. - Add a new "myClasifier.py" for further split-testing. - Use the "title" from dataSet, more times, for improved accuracy.

--- rfClassifier.py
+++ rfClassifier.py
@@ -7,16 +7,44 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn import preprocessing
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 import pandas as pd
 from sklearn.preprocessing import Normalizer
 
-if __name__ == '__main__':
-    train_data = pd.read_csv('train_set.csv', sep="\t")
-    testdata = pd.read_csv('test_set.csv', sep="\t")
 
-    train_data = train_data[0:25]
-    testdata = testdata[0:25]
+
+
+
+def split_dataset(dataset, train_percentage, feature_headers, target_header):
+    """
+    Split the dataset with train_percentage
+    :param dataset:
+    :param train_percentage:
+    :param feature_headers:
+    :param target_header:
+    :return: train_x, test_x, train_y, test_y
+    """
+
+    # Split dataset into train and test dataset
+    train_x, test_x, train_y, test_y = train_test_split(dataset[feature_headers], dataset[target_header],
+                                                        train_size=train_percentage)
+    return train_x, test_x, train_y, test_y
+
+
+
+if __name__ == '__main__':
+
+    headers = ['RowNum', 'Id', 'Title', 'Content', 'Category']
+
+    train_data = pd.read_csv('train_set.csv', sep="\t")
+    test_data = pd.read_csv('test_set.csv', sep="\t")
+
+    train_x, test_x, train_y, test_y = split_dataset(train_data, 0.7, headers[2:-1], headers[-1])
+
+
+   # train_data = train_data[0:250]
+   # test_data = test_data[0:250]
 
     le = preprocessing.LabelEncoder()
     le.fit(train_data["Category"])
@@ -24,7 +52,7 @@ if __name__ == '__main__':
 
     # print y
     count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
-    tfidf = TfidfTransformer()
+    #tfidf = TfidfTransformer()
     clf = RandomForestClassifier(n_estimators=50)
 
     # vX = count_vectorizer.fit_transform(train_data)
@@ -41,9 +69,18 @@ if __name__ == '__main__':
         ('clf', RandomForestClassifier()),
     ])
 
-    vectorTrain = count_vectorizer.fit_transform(train_data['Content'], train_data['Title'])
-    vectorTest = count_vectorizer.fit_transform(testdata['Content'], testdata['Title'])
-    transformer = TfidfTransformer(smooth_idf=False)
+
+    print train_data.shape
+
+    for row in train_data:
+        train_data['Content'] += 5 * train_data['Title']
+
+    for row in test_data:
+        test_data['Content'] += 5 * test_data['Title']
+
+    vectorTrain = count_vectorizer.fit_transform(train_data['Content'])
+    vectorTest = count_vectorizer.fit_transform(test_data['Content'])
+    # transformer = TfidfTransformer(smooth_idf=False)
 
     print vectorTrain.shape
     print vectorTest.shape
@@ -51,11 +88,11 @@ if __name__ == '__main__':
 
     lsa = TruncatedSVD(n_components=100)
 
-    vectorTrain = lsa.fit_transform(vectorTrain)
-    vectorTrain = Normalizer(copy=False).fit_transform(vectorTrain)
+    #vectorTrain = lsa.fit_transform(vectorTrain)
+    # vectorTrain = Normalizer(copy=False).fit_transform(vectorTrain)
 
-    vectorTest = lsa.fit_transform(vectorTest)
-    vectorTest = Normalizer(copy=False).fit_transform(vectorTest)
+    #vectorTest = lsa.fit_transform(vectorTest)
+    # vectorTest = Normalizer(copy=False).fit_transform(vectorTest)
 
     print vectorTrain.shape
     print vectorTest.shape
@@ -78,4 +115,3 @@ if __name__ == '__main__':
     print "Train Accuracy :: ", accuracy_score(y, clf.predict(vectorTrain))
     print "Test Accuracy  :: ", accuracy_score(y, y_pred)
 
-    # train_test_split()
